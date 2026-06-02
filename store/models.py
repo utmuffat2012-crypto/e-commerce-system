f87b44edd96ee991dade78d343368126e0c49c32
utibemuffat <utmuffat2012@gmail.com>
2026-06-02 03:24:41 +0100

    Fix models.py error

--- store/models.py
+++ store/models.py
@@ -39,11 +39,3 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return self.product.name
-
-    class Cart(models.Model):
-        user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    class CartItem(models.Model):
-        cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-        product = models.ForeignKey(Product, on_delete=models.CASCADE)
-        quantity = models.PositiveIntegerField(default=1)
