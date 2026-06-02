2e10973822cf97b3f2968c9cc93e6746d70c5b4d
utibemuffat <utmuffat2012@gmail.com>
2026-06-02 03:01:34 +0100

    Fix homepage product display

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -27,3 +27,12 @@ urlpatterns = [
 urlpatterns += static(settings.MEDIA_URL,
 document_root=settings.MEDIA_ROOT)
 
+from django.contrib.auth import views as auth_views
+
+urlpatterns += [
+    path('login/', auth_views.LoginView.as_view(
+        template_name='store/login.html'
+    ), name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
